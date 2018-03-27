7daa6e41dc273798962a549fca87dd129095f440
stumash <stuart.mashaal@gmail.com>
2018-03-26 21:22:35 -0400

    more work on synchronized queue

--- 03/q1/Queue.java
+++ 03/q1/Queue.java
@@ -1,7 +1,4 @@
 public interface Queue {
-    private boolean verbose;
-    public Queue(boolean verbose);
     public void enqueue(QueueElement elem);
     public QueueElement dequeue();
-    private void printlnIfVerbose(String s);
 }
--- 03/q1/SynchronizedQueue.java
+++ 03/q1/SynchronizedQueue.java
@@ -4,11 +4,6 @@ import java.util.ArrayDeque;
 public class SynchronizedQueue imlements Queue {
     private Deque<Integer> queue = new ArrayDeque<>();
 
-    public SynchronizedQueue(boolean verbose)
-    { this.verbose = verbose; }
-    private void printlnIfVerbose(String s)
-    { if (this.verbose) { System.out.println(s); } }
-
     public void enqueue(QueueElement elem) {
         synchronized(this) {
             elem.timeEnqueued = System.currentTimeMillis();
