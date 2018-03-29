4110a1d882ac1f022f5a58930b096143ac070040
stumash <stuart.mashaal@gmail.com>
2018-03-29 07:09:16 -0400

    still need to implement the algorithm

--- 03/q2/Main.java
+++ 03/q2/Main.java
@@ -40,9 +40,8 @@ public class Main {
         long startTime = System.currentTimeMillis();
 
         while (conflicting.size() > 0) {
-            break; // for now while debugging
-            //   t assign threads
-            //   t detect conflict
+            break;
+            // TODO: the algorithm
         }
 
         System.out.println(
